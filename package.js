8086d98efbcac334e700db070f2a345d647c2b0f
David Baxter <davebaxter@gmail.com>
2016-10-21 10:17:03 +1100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'supaseca:camera-ui',
-    version: '0.0.51',
+    version: '0.0.52',
     summary: 'Updated for Meteor 1.4. Take photos with UI on desktop and mobile with camera or photo roll.',
     git: 'https://github.com/projik/camera-ui',
     documentation: 'README.md'
