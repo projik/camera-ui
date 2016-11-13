60c3929821c3d14bb4669f6ebf6a8dbcc35768c2
David Baxter <davebaxter@gmail.com>
2016-11-13 14:23:27 +1100

    update actionsheet plugn update camera plugin version

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
     name: 'supaseca:camera-ui',
-    version: '0.0.52',
+    version: '0.0.54',
     summary: 'Updated for Meteor 1.4. Take photos with UI on desktop and mobile with camera or photo roll.',
     git: 'https://github.com/projik/camera-ui',
     documentation: 'README.md'
 });
 
 Cordova.depends({
-    "nl.x-services.plugins.actionsheet": "https://github.com/EddyVerbruggen/cordova-plugin-actionsheet/tarball/df5e89e54d5142c964c1139db6fe813a01125825",
+    "cordova-plugin-actionsheet": "2.3.1",
     "org.apache.cordova.device": "1.1.3"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.3');
     api.export('MeteorCameraUI');
-    api.use('supaseca:camera@1.4.2');
+    api.use('supaseca:camera@1.4.3');
     api.addFiles('camera-ui.js');
     api.addFiles('camera-ui-client.js', ['web.browser', 'web.cordova']);
     api.addFiles('camera-ui-browser.js', ['web.browser']);
